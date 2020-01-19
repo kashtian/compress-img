3e8277fc24dc7a439a1b7023e33a4f9d816787aa
tianshi <tianshi@snqu.com>
2020-01-19 14:08:23 +0800

    imagemin instead of gm

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 图片压缩
 
 ## 简介
-使用gm进行图片的压缩处理
+使用imagemin进行图片的压缩处理
 
 ## 说明
-`npm install` 前需要安装**GraphicsMagick**
+`npm install`
 
 ## 使用
 `node ./ -h` 查看帮助信息
--- index.js
+++ index.js
@@ -1,157 +1,135 @@
-const fs = require('fs');
+const fs = require('fs')
 const path = require('path');
-const gm = require('gm');
 const colors = require('colors');
 const imagemin = require('imagemin');
 const imageminPng = require('imagemin-pngquant');
+const imageminJpeg = require('imagemin-mozjpeg')
+const losslessJpeg = require('imagemin-jpegtran')
+const losslessPng = require('imagemin-optipng')
 
 colors.setTheme({
-    error: 'red',
-    help: 'cyan',
-    success: 'green'
+  error: 'red',
+  help: 'cyan',
+  success: 'green'
 })
 
 let commands = {
-    '-h, --help': '帮助信息',
-    '-c, --compress': '进行图片压缩',
-    '-s, --source [absolute path]': '需要压缩的图片目录地址（绝对路径）',
-    '-d, --dest [absolute path]': '图片压缩后存储的目录地址（绝对路径）',
-    '-q, --quality [number]': '图片压缩的质量选择0-100,默认88',
-    '-qp, --quality_png [number]': '指定png图片压缩质量0-100',
-    '-qj, --quality_jpg [number]': '指定jpg图片压缩质量0-100',
+  '-h, --help': '帮助信息',
+  '-s, --source [path]': '非绝对路径会从当前工作目录查找',
+  '-d, --dest [path]': '非绝对路径会从当前工作目录查找',
+  '-qp, --quality_png [min: number, max: number]': '指定png图片压缩质量0-1, 默认0.8,0.9',
+  '-qj, --quality_jpg [number]': '指定jpg图片压缩质量0-100, 默认90',
 }
 let options = {
-        source: './images',
-        dest: './dest',
-        quality: 88
-    }, 
-    ckMap = keyMap();
+  source: './img',
+  dest: './img-dest'
+}
+let comMap = keyMap()
 
 // 获取用户输入的命令，并做相应检查
 function init() {
-    let keys = Object.keys(commands).join(';').match(/(-+\w+)/g);
-    let rKey = '', value = '', key = '';
-    for (let i = 0, len = process.argv.length; i < len; i++) {
-        key = process.argv[i];
-        if (!/^-/.test(key)) {
-            continue;
-        }
-        if (keys.includes(key)) {
-            rKey = key.replace(/-+/, '');
-            value = getArg(key);
-
-            if (checkAbPathByKey(ckMap[rKey] || rKey, value)) {
-                options[ckMap[rKey] || rKey] = value;
-            } else {
-                console.log(`${key}只能输入绝对路径`.error);
-                return;
-            }                   
-        } else {
-            options.help = true;
-            console.log(`${key}命令不存在，请查看帮助信息`.error);
-            break;
-        }
+  let comKey = '', value = '', argvItem = '';
+  for (let i = 0, len = process.argv.length; i < len; i++) {
+    argvItem = process.argv[i];
+    if (!/^-/.test(argvItem)) {
+      continue;
     }
-    startTask();
+    comKey = getCommandKey(argvItem);
+    if (comKey) {
+      value = getArgByIndex(i + 1);
+      if (['source', 'dest'].includes(comKey)) {
+        value = path.resolve(value)
+        if (!fs.existsSync(value)) {
+          return console.log(`${value} 目录不存在`.error)
+        }
+      }
+      options[comKey] = value
+    } else {
+      options.help = true;
+      console.log(`${argvItem}命令不存在，请查看帮助信息\n`.error);
+      break;
+    }
+  }
+  startTask();
 }
 init();
 
 function startTask() {
-    if (options.help) {
-        printCommand();
-    } else if (options.compress) {
-        compress();
-    }
+  if (options.help) {
+    return printCommand();
+  }
+  compressImg()
 }
 
 // 压缩图片
-function compress() {
-    fs.readdir(options.source, (err, files) => {
-        if (err) {
-            console.log(err.message.error)
-            return;
-        }
-
-        files.forEach(file => {
-            if (/^\./.test(file)) {
-                return;
-            }
-            if (/\.png/.test(file)) {
-                compressPng(file);
-            } else {
-                compressSingleFile(file);
-            }            
-        })       
-        
-    })    
+function compressImg() {
+  let dirPath = path.join(options.source, '/*.{jpg,png}').replace(/\\/g, '/')
+  imagemin([dirPath], {
+    destination: `${options.dest}`,
+    plugins: [
+      getJpgPlugin(),
+      getPngPlugin()
+    ]
+  }).then(files => {
+    console.log(`compress ${dirPath} success`.success)
+  }).catch(err => {
+    console.log(`compress ${dirPath} error: ${err}`.error)
+  })
 }
 
-// 根据文件名压缩单个文件(jpg)
-function compressSingleFile(fileName) {
-    gm(`${options.source}/${fileName}`)
-    .quality(options.quality_jpg || options.quality)
-    .write(`${options.dest}/${fileName}`, function(err) {
-        if (err) {
-            console.log(`compress ${fileName} error: ${err}`.error)
-        } else {
-            console.log(`compress ${fileName} success`.success)
-        }
-    })
+// 获取jpg插件
+function getJpgPlugin() {
+  return options.quality_jpg ? imageminJpeg({
+    quality: options.quality_jpg === true ? 90 : options.quality_jpg
+  }) : losslessJpeg({
+    progressive: true
+  })
 }
 
-// 压缩png图片
-function compressPng(fileName) {
-    imagemin([`${options.source}/${fileName}`], `${options.dest}`, {
-        plugins: [
-            imageminPng({
-                quality: options.quality_png || options.quality
-            })
-        ]
-    }).then(files => {
-        console.log(`compress ${fileName} success`.success)
-    }).catch(err => {
-        console.log(`compress ${fileName} error: ${err}`.error)
-    })
-}
-
-// 检查用户输入的路径是否是绝对路径
-function checkAbPathByKey(key, value) {
-    if (['source', 'dest'].includes(key)) {
-        if (typeof value != 'string') {
-            return false;
-        }
-        return path.isAbsolute(value);
-    }
-    return true;
+// 获取png插件
+function getPngPlugin() {
+  return options.quality_png ? imageminPng({
+    quality: options.quality_png === true ? [0.8,0.9] : options.quality_png
+  }) : losslessPng({
+    optimizationLevel: 7
+  })
 }
 
 // 获取缩写对应的命令map
 function keyMap() {
-    let map = {};
-    for (let key in commands) {
-        if (/-(\w+).*--(\w+)/.test(key)) {
-            map[RegExp.$1] = RegExp.$2;
-        }
+  let map = {};
+  for (let key in commands) {
+    if (/-(\w+).*--(\w+)/.test(key)) {
+      map[RegExp.$1] = RegExp.$2;
+      map[RegExp.$2] = RegExp.$2
     }
-    return map;
+  }
+  return map;
+}
+
+function getCommandKey(argv) {
+  let key = argv.replace(/-+/, '')
+  return comMap[key]
 }
 
 // 获取cli里配置的参数
-function getArg(name) {
-    let index = process.argv.indexOf(name);
-    if (index < 0) {
-        return;
-    }
-    let value = process.argv[index + 1];
-    if (!value || /^-/.test(value)) {
-        return true;
-    }
-    return value;
+function getArgByIndex(index) {
+  let value = process.argv[index];
+  if (!value || /^-/.test(value)) {
+    return true;
+  }
+  if (value.includes(',')) {
+    value = value.split(',')
+    value = value.map(item => {
+      return parseFloat(item)
+    })
+  }
+  return value;
 }
 
 // 打印命令信息
 function printCommand() {
-    for (let k in commands) {
-        console.log(`${k}   ${commands[k]}`.help)
-    }    
+  for (let k in commands) {
+    console.log(`${k}   ${commands[k]}`.help)
+  }
 }
